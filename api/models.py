0031e6b000c256df9e9f6f9919c389d92afec2b8
moffire <alexeydudarev.spb@gmail.com>
2020-12-25 18:26:22 +0300

    created models for workplace reserving

---
+++ api/models.py
@@ -0,0 +1,31 @@
+# from django.core.exceptions import ValidationError
+from django.db import models
+from django.db.models import Q
+from rest_framework.exceptions import ValidationError
+
+
+class Workplace(models.Model):
+	pass
+
+	def __str__(self):
+		return f'Рабочее место №{self.pk}'
+
+
+class Reservation(models.Model):
+	workplace = models.ForeignKey(Workplace, on_delete=models.CASCADE, related_name='reservations', verbose_name='Рабочее место')
+	datetime_from = models.DateTimeField(verbose_name='Дата начала брони')
+	datetime_to = models.DateTimeField(verbose_name='Дата окончания брони')
+
+	def clean(self, *args, **kwargs):
+		if self.datetime_from >= self.datetime_to :
+			raise ValidationError('Дата начала брони не может быть больше или равна дате окончания')
+
+		# checking date intersection with exists reservations
+		workplace = Workplace.objects.get(pk=self.workplace.pk)
+		intersections = workplace.reservations.filter(Q(datetime_from__range=[self.datetime_from, self.datetime_to]) |
+		                                              Q(datetime_to__range=[self.datetime_from, self.datetime_to]) |
+		                                              Q(datetime_from__lte=self.datetime_from, datetime_to__gte=self.datetime_to))
+		if intersections:
+			raise ValidationError('Введенные даты забронированы')
+
+
