9b0636fe7a885e9cbd2788b74216b906fa5e7ad1
moffire <alexeydudarev.spb@gmail.com>
2020-12-27 17:48:12 +0300

    added BasicAuthentication

--- LIIS/settings.py
+++ LIIS/settings.py
@@ -131,6 +131,7 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly'
